36a9f3b2f1296ef2f8811b50de4b893ea815b2c4
Olive <oliverachermann23@gmail.com>
2021-11-05 09:47:29 +0100

    more testcases added

--- TravelingSalesman/src/test/java/PathFinderTest.java
+++ TravelingSalesman/src/test/java/PathFinderTest.java
@@ -1,22 +1,45 @@
 import junit.framework.Assert;
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.jupiter.api.BeforeAll;
 
-import java.util.ArrayList;
 import java.util.TreeMap;
 
 public class PathFinderTest {
 
-    private TreeMap<String, int[]> cityMap;
+    private static TreeMap<String, int[]> cityMap;
+    private static PathFinder pf;
 
 
-    @Test
-    public void test(){
+    @Before
+    public void init(){
         cityMap = new TreeMap<>();
         cityMap.put("Zürich", new int[]{8, 8});
         cityMap.put("Argau", new int[]{1, 1});
         cityMap.put("Luzern", new int[]{3, 3});
-        PathFinder pf = new PathFinder();
+        pf = new PathFinder();
+    }
+
+    @Test
+    public void testShortestPathStartingPoint(){
         int[] result = pf.findShortestPath(cityMap, 0).get(0);
+        Assert.assertEquals(1,result[0]);
+    }
+    @Test
+    public void testShortestPathSecondPoint(){
+        int[] result = pf.findShortestPath(cityMap, 0).get(1);
+        Assert.assertEquals(3,result[0]);
+    }
+
+    @Test
+    public void testShortestPathThirdPoint(){
+        int[] result = pf.findShortestPath(cityMap, 0).get(2);
+        Assert.assertEquals(8,result[0]);
+    }
+
+    @Test
+    public void testShortestPathEndPoint(){
+        int[] result = pf.findShortestPath(cityMap, 0).get(3);
         Assert.assertEquals(1,result[0]);
     }
 
